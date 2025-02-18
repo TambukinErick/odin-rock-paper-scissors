71f64fb5d0cd9d52c12343ff653f5e9e769ac200
erick <124009200+TambukinErick@users.noreply.github.com>
2025-02-18 15:53:13 +0800

    Add game functionality by replacing main game loop with a series of button listeners that calls the play round function. Additional functionality is given to th eplay round function by checking for winners and displaying the round information

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
 let computerScore = 0;
 let humanScore = 0;
+let gameOngoing = true;
+
+const display = document.querySelector("#game_display");
+const rockBtn = document.querySelector("#btn-rock");
+const paperBtn = document.querySelector("#btn-paper");
+const scissorBtn = document.querySelector("#btn-scissors");
+let gameLog = ``;
+
+rockBtn.addEventListener("click", () => {
+  if (gameOngoing) {
+    playRound("rock", getComputerChoice());
+  }
+});
+
+paperBtn.addEventListener("click", () => {
+  console.log("clicking paper");
+  if (gameOngoing) {
+    playRound("paper", getComputerChoice());
+  }
+});
+
+scissorBtn.addEventListener("click", () => {
+  if (gameOngoing) {
+    playRound("scissors", getComputerChoice());
+  }
+});
+
+
+function playRound(humanChoice, computerChoice) {
+  const result = determineRoundWinner(humanChoice, computerChoice);
+  updateScores(result);
+  gameLog += generateRoundResultMessage(result, humanChoice, computerChoice);
+  gameLog += checkForWinner();
+  game_display.textContent = gameLog;
+}
 
 function getComputerChoice() {
   let choice;
   const val = Math.floor(Math.random() * 3);
   
-  console.log(val);
-
   if(val == 1) choice = 'rock';
   if(val == 2) choice = 'paper';
   if(val == 0) choice = 'scissors';
 
   return choice;
-}
-
-
-function getHumanChoice() {
-  let choices = ['rock', 'paper', 'scissors'];
-  let validInputStatus = false;
-  let result;
-
-  while (validInputStatus == false){
-    let humanChoiceInput = prompt("Please type one of the following: rock, paper, or scissors");
-    result = humanChoiceInput.toLowerCase();
-    if (choices.includes(result)) { break; }
-  }
-
-  return result;
 }
 
 function determineRoundWinner(humanChoice, computerChoice) {
@@ -41,13 +59,6 @@ function determineRoundWinner(humanChoice, computerChoice) {
     return "win"
 }
 
-function generateRoundResultMessage(result, humanChoice, computerChoice) {
-  if (result == "tie") return `Its a tie, you both played ${humanChoice}`;
-  if (result == "lose") return `You lose! ${computerChoice} beats ${humanChoice}`;
-  if (result == "win") return `You win! ${humanChoice} beats ${computerChoice}`;
-
-}
-
 function updateScores(result) {
   if (result == "win") {
     humanScore += 1;
@@ -56,31 +67,29 @@ function updateScores(result) {
   }
 }
 
-function playRound(humanChoice, computerChoice) {
-  const result = determineRoundWinner(humanChoice, computerChoice);
-  updateScores(result);
+function generateRoundResultMessage(result, humanChoice, computerChoice) {
+  if (result == "tie") return `Its a tie, you both played ${humanChoice}\r\n`;
+  if (result == "lose") return `You lose! ${computerChoice} beats ${humanChoice}\r\n`;
+  if (result == "win") return `You win! ${humanChoice} beats ${computerChoice}\r\n`;
 
-  return generateRoundResultMessage(result, humanChoice, computerChoice);
 }
 
+function checkForWinner() {
+  if (computerScore == 5 || humanScore == 5) {
+    gameOngoing = false;
+    return getFinalWinner();
+  }
+  return "";
+}
 function getFinalWinner() {
   if (humanScore > computerScore) {
-    return `You Win!\nFinal Scores\nHuman:${humanScore} Computer:${computerScore} `;
+    return `You Win!\nFinal Scores\nHuman:${humanScore} Computer:${computerScore}\n`;
   } else if (humanScore < computerScore) {
-    return `You Lose! Better Luck Next Time\nFinal Scores\nHuman:${humanScore} Computer:${computerScore}`;
+    return `You Lose! Better Luck Next Time\nFinal Scores\nHuman:${humanScore} Computer:${computerScore}\n`;
   } else {
-    return `Its A Tie\nFinal Scores\nHuman:${humanScore} Computer:${computerScore}` ;
+    return `Its A Tie\nFinal Scores\nHuman:${humanScore} Computer:${computerScore}\n` ;
   }
 }
 
-function startGame() {
-  for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    console.log(`round #${i+1}`)
-    console.log(playRound(humanSelection, computerSelection));
-  }
-}
 
-startGame();
-console.log(getFinalWinner());
+
